1d1269e1df7013998fab97d1f847af35f33f3ead
Keno Krieger <kriegerk@uni-bremen.de>
2021-09-23 11:37:38 +0200

    Add size scaling

--- mkconfig.py
+++ mkconfig.py
@@ -7,17 +7,31 @@ TSTART = 0.2
 TEND = 0.6
 TSTEP = 0.02
 TESTRUNS = 5
-
+SIZESTART = 32
+SIZEEND = 1024
+SIZEMULTIPLY = 2
 
 if __name__ == "__main__":
-    with open("pymarket.conf", "r") as f:
-        multising_config = f.read()
+    multising_config = """#
+grid_depth = {0}
+grid_width = {0}
+grid_height = {0}
+alpha = 0.0
+j = 1.0
+total_updates = 5000
+beta = {1}
+seed = {2}
+"""
 
     for run in range(TESTRUNS):
-        seed = f"\nseed = {randrange(1, 1000000)}"
-        for temp in arange(TSTART, TEND, TSTEP):
-            config_temp = "\nbeta = {}".format(temp)
+        size = SIZESTART
+        seed = randrange(1, 1000000)
 
-            file_id = "{}/run={}_temp={:.3f}.conf".format(argv[1], run, temp)
-            with open(file_id, "w") as f:
-                f.write(multising_config + seed + config_temp)
+        while size <= SIZEEND:
+            for temp in arange(TSTART, TEND, TSTEP):
+
+                file_id = "{}/run={}_size={}_temp={:.3f}.conf".format(argv[1], run, size, temp)
+                with open(file_id, "w") as f:
+                    f.write(multising_config.format(size, temp, seed))
+
+            size *= SIZEMULTIPLY
